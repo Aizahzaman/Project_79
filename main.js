227510bf7c3aafc8a61e77dae5be6bb29ffde2d7
Aizahzaman <79235506+Aizahzaman@users.noreply.github.com>
2021-05-25 21:55:56 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ function getmenu(){
     for(var i=0;i<menu_list_array.length;i++){
     htmldata=htmldata+'<li>'+menu_list_array[i]+'</li>';
     }
-    htmldata=htmldata="</ol>"
-    document.getElementById("display_menu").innerHTML=menu_list_array;
+    htmldata=htmldata+"</ol>"
+    document.getElementById("display_menu").innerHTML=htmldata;
 }
 
 function add_item(){
@@ -21,9 +21,8 @@ var item=document.getElementById("add_item").value;
 menu_list_array.sort();
 htmldata="<section class='cards'>"
 for(var i=0;i<menu_list_array.length;i++){
-    htmldata=htmldata+'<div class="card">'
-    + '<img src="images/pizzaImg.png"/>'
-    + menu_list_array[i] + '</div>'
+
+    htmldata=htmldata+'<div class="card">'+ '<img src="images/pizzaImg.png">'+ menu_list_array[i] + '</div>'
 }
 htmldata=htmldata+"</section>"
 document.getElementById("display_addedmenu").innerHTML=htmldata;
@@ -32,5 +31,5 @@ document.getElementById("display_addedmenu").innerHTML=htmldata;
 function add_top(){
 var item=document.getElementById("add_item").value;
 menu_list_array.push(item);
-add_item()
+add_item();
 }
